a11a2d131e928486ab8099bae19609ed832366fd
Ramanujan <hararecreg@gmail.com>
2022-11-29 20:00:50 -0600

    Initial Commit

---
+++ SwiftNotes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftNotes
+//
+//  Created by Hararec Medina González on 29/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
