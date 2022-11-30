107cec120b756a7d6073127625fc19dd66a627e9
Ramanujan <hararecreg@gmail.com>
2022-11-29 20:14:03 -0600

    Se agregaron tipos de datos

---
+++ SwiftNotes/TiposDeDatos.playground/Contents.swift
@@ -0,0 +1,25 @@
+import UIKit
+
+/*
+ *comentario
+ Se busca un lenguage sencillo, por eso se omiten ;
+ Se usa lower camel case
+ Se obtiene la info de una variable pulsando Option + click sobre una variable
+*/
+
+//Strings
+var greeting = "playground", greeting2 = "Hello"
+let palabraCompuesta: String = greeting + greeting2
+let banner = """
+          __,
+         (           o  /) _/_
+          `.  , , , ,  //  /
+        (___)(_(_/_(_ //_ (__
+                     /)
+                    (/
+        """
+greeting2 = "greeting \(greeting)"
+
+
+//Numbers
+var cero: Int = 0, primerosDigitosE: Double = 2.71828
